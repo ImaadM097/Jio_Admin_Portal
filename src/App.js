8ae327937878c5fa74b518aca1f3f0e2bfda53d5
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-06-26 11:04:03 +0530

    Update App.js to resolve merge conflict

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import Sidebar from "./dashboard/Sidebar/Sidebar";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './dashboard/navbar.jsx'
 
 function App() {
-  return <Sidebar />;
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
 }
 
 export default App;
