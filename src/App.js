c20d1e620ccc6096459b1e677e2dd07c147f08e6
Bhaarath Karthikeyan <56688291+bk-147@users.noreply.github.com>
2023-06-26 11:14:40 +0530

    added route to dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from './login/login';
+import Navbar from './dashboard/navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Navbar />} />
+
       </Routes>
     </BrowserRouter>
   );
