b9a4ebc3da816f156f79f888d2668c71d4afdd44
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-06-27 16:48:03 +0530

    Made changes to navbar

--- src/dashboard/navbar.js
+++ src/dashboard/navbar.js
@@ -3,30 +3,17 @@ import './navbar.css'
 const Navbar = () => {
     return (
     <div id='mainNavbarDiv'>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" id='mainNav'>
-  <div class="container-fluid" id ='mainNavDiv'>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+        <div class="dropdown">
+        <img src='/images/user-01.png' id='profileImg'></img>
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    John Doe
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Profile</a>
+    <a class="dropdown-item" href="#">Account Setting</a>
+    <a class="dropdown-item" href="#">Logout</a>
   </div>
-</nav>
+</div>
     </div>
     );
 
