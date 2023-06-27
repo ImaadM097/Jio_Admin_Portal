927575d255e9f042f5d03a0d3ac6fad39d0b2fbb
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-06-27 20:34:25 +0530

    Added table component

---
+++ src/dashboard/Tables/TableData.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './tables.css'
+
+const TableRow = ({data, index}) => {
+    const comments = data.comment
+
+    return (
+        <>
+            <tr>
+                <td>{data.channel_name}</td>
+                <td>{data.title}</td>
+                <td>
+                    <button className='btn btn-primary sm' data-bs-toggle="modal" data-bs-target={`#modal${index}`}>{data.comment_count}</button>
+                    <div className='modal fade' id={`modal${index}`}>
+                        <div className='modal-dialog modal-dialog-centered'>
+                            <div className='modal-content'>
+                                <div className='modal-header'>
+                                    <h2>View Comments</h2>
+                                </div>
+                                <div className='modal-body'>
+                                    <ul>
+                                    {
+                                        data.comment.map((com)=>{
+                                            return (
+                                                <p>{com}</p>
+                                            )
+                                        })
+                                    }
+                                    </ul>
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </td>
+                <td>{data.inappropriate_comment_count}
+                </td>
+                <td><img src="" alt="thumbnail"/></td>
+            </tr>
+        </>
+    );
+}
+
+export default TableRow
--- src/dashboard/Tables/Tables.js
+++ src/dashboard/Tables/Tables.js
@@ -1,16 +1,66 @@
 import React, { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import Navbar from '../navbar';
+import './tables.css'
+import response from './response.json'
+import TableRow from './TableData';
 
 
 const Tables = () => {
 
+    const videos = response.data.videos;
+    const totalCount = response.data.total;
+    const count = (totalCount > 10) ? 10 : totalCount;
+    const newVideos = videos.slice(0, count-1);
+
+
+
     return (
         <>
-            <div className='mainContainer'>
-                <Sidebar />
-                <Navbar />
+            <div className='mainContainer' id="mainTable">
+                <Sidebar id="SidebarTable"/>
+                <div className='RightSide'>
+                    <Navbar id="NavbarTable"/>
+                    <div className="tablediv">
+                        <form>
+                        <div className='input-group' id="searchBar">
+                            <input type='text' className='form-control form-control-md' placeholder='Search...'/>
+                            <button className='btn btn-primary'>GO</button>
+                        </div>
+                        </form>
+                        <div className='container'>
+                            <h4>Videos</h4>
+                        </div>
+                            <div className='container' id='tableContainer'>
+                                <table className='table'>
+                                    <thead>
+                                        <tr>
+                                            <th>Channel</th>
+                                            <th>Video Title</th>
+                                            <th>Comment Count</th>
+                                            <th>In-Appropriate Comment Count</th>
+                                            <th>Thumbnail</th>
+                                        </tr>
+                                    </thead> 
+                                    <tbody>
+                                    {(response == null)? (
+                                        <h5>No data</h5>
+                                    ):(
+                                        newVideos.map((item, index)=>{
+                                            return (
+                                                <TableRow data={item} index={index}/>
+                                            )
+                                        })
+                                    )
+                                    
+                                    }
+                                    </tbody>
+                                </table>
+                            </div>
+                    </div>
+                </div>
             </div>
+            
         </>
     )
 }
