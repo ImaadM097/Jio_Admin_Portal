f7be8512eec88e2621ffede775d5909478581e2c
Nandan007S <111338271+Nandan007S@users.noreply.github.com>
2023-06-30 18:10:47 +0530

    Thumbnail icon added

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../styles/tables.css'
-
 const TableRow = ({data, index}) => {
-    
-
     return (
         <>
             <tr>
@@ -27,19 +24,38 @@ const TableRow = ({data, index}) => {
                                         })
                                     }
                                     </ul>
-                                
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
                 </td>
                 <td>{data.inappropriate_comment_count}
                 </td>
-                <td><img src="" alt="thumbnail"/></td>
+                <td id='thumbnail'> 
+                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img src='/images/thumbnailLogo.png' width={'25'} height={'25'}></img>
+                    </button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+                
+                </td>
             </tr>
         </>
     );
 }
-
 export default TableRow
