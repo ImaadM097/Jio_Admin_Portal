cf00d14fc37a057cbad9303cbddc30d4e924949e
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-06-30 20:01:40 +0530

    Added video player

--- src/components/VideosTableRow.js
+++ src/components/VideosTableRow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 import '../styles/tables.css'
 
 const TableRow = ({data, index}) => {
@@ -11,7 +12,22 @@ const TableRow = ({data, index}) => {
                 <td>{data.tenant}</td>
                 <td>{data.status}</td>
                 <td>{data.duration}</td>
-                <td>{data.video}</td>
+                <td>
+                    <button className='btn btn-primary sm' data-bs-toggle="modal" data-bs-target={`#modal${index}`}>Play Video</button>
+                    <div className='modal fade' id={`modal${index}`}>
+                        <div className='modal-dialog modal-dialog-centered modal-lg'>
+                            <div className='modal-content'>
+                                <div className='modal-header'>
+                                    <h2>Play Video</h2>
+                                    <button className='btn-close' data-bs-dismiss="modal" data-bs-target={`#modal${index}`}></button>
+                                </div>
+                                <div className='modal-body' id='videoPlayer'>
+                                    <ReactPlayer url={data.video} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
             </tr>
         </>
     );
