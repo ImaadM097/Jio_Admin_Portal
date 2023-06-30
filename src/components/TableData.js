2ab5afed92fba5d94c26f7a48badbe5b0723e86b
Nandan007S <111338271+Nandan007S@users.noreply.github.com>
2023-06-30 18:17:08 +0530

    tn

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -39,7 +39,7 @@ const TableRow = ({data, index}) => {
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Thumbnail</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
