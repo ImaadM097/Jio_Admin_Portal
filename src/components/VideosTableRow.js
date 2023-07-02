3978f07dbd0eb8b9fa40deb8a113b0d2e4840dfc
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-07-02 21:35:12 +0530

    Minor change to play button

--- src/components/VideosTableRow.js
+++ src/components/VideosTableRow.js
@@ -13,7 +13,7 @@ const TableRow = ({data, index}) => {
                 <td>{data.status}</td>
                 <td>{data.duration}</td>
                 <td>
-                    <button className='btn btn-primary sm' data-bs-toggle="modal" data-bs-target={`#modal${index}`} id="playVideoBtn">
+                    <button className='btn' data-bs-toggle="modal" data-bs-target={`#modal${index}`} id="playVideoBtn">
                         <img src='/images/video-play-icon-1.jpg' width={'25'} height={'25'}></img>
                     </button>
                     <div className='modal fade' id={`modal${index}`}>
