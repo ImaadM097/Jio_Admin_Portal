c6429a86a339b14181c3bbdd52e80e466f481778
Nandan007S <111338271+Nandan007S@users.noreply.github.com>
2023-07-04 13:25:13 +0530

    Update Router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -9,7 +9,7 @@ function Router() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={
             <>
               <Dashboard />
