6b598f0add940b40d25dc4ee5d549a67f248c252
Nandan007S <111338271+Nandan007S@users.noreply.github.com>
2023-07-05 18:34:12 +0530

    Update Homepage.js

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+
 import { useNavigate } from 'react-router-dom';
 export default function Homepage(){
     const navigate = useNavigate();
