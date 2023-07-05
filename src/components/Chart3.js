194d830133cc9309d97fc6e981e7ef64ccf59354
Bhaarath Karthikeyan <56688291+bk-147@users.noreply.github.com>
2023-07-05 11:21:33 +0530

    added apis to line and pie chart

--- src/components/Chart3.js
+++ src/components/Chart3.js
@@ -4,20 +4,58 @@ import Chart from "react-apexcharts";
 class Chart3 extends Component {
     constructor(props) {
         super(props);
-
+        this.updateCharts = this.updateCharts.bind(this);
         this.state = {
-            options: {},
-            series: [44, 55, 41, 17, 15],
-            labels: ['A', 'B', 'C', 'D', 'E']
+            series: [44, 55],
+            options: {
+            
+            labels: ['Active', 'Inactive']}
         }
+    }
+
+    async updateCharts() {
+        const newSeries = [];
+        
+        
+
+        const url = new URL('https://649f0fa3245f077f3e9d4cf3.mockapi.io/Tenants')
+        
+
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: { 'content-type': 'application/json' }
+        })
+        const newdata = await res.json()
+        
+        let active = newdata.filter( (obj)=>{
+            return obj.status
+        })
+        let inactive = newdata.filter( (obj)=>{
+            return !obj.status
+        })
+
+        
+
+
+        newSeries.push(active.length)
+        newSeries.push(inactive.length)
+        console.log(newSeries)
+        this.setState({
+            series: newSeries,
+            
+          })
     }
 
     render() {
         return (
             <div className="chart-container">
+                
                 <div className="donut">
                     <Chart options={this.state.options} series={this.state.series} type="donut" />
                 </div>
+                <p className="col">
+                <button onClick={this.updateCharts}>Update!</button>
+                </p>
             </div>
         );
     }
