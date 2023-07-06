0b87dad156ed598ec7258803b44c9795e28a4760
Bhaarath Karthikeyan <56688291+bk-147@users.noreply.github.com>
2023-07-06 23:30:50 +0530

    added create tenant

--- src/components/CreateTenant.js
+++ src/components/CreateTenant.js
@@ -6,39 +6,58 @@ const CreateTenant = ()=>{
     const navigate = useNavigate();
     const [formData,setFormData] = useState({
         name:"",
-        status:true,
-        domain:""
+        active:true,
+        domain:"",
+        
     });
+    const [volumeControlEnabled, setVolumeControlEnabled] = useState(false);
+    const [productDrawerEnabled, setProductDrawerEnabled] = useState(false);
+    const [reportEnabled, setReportEnabled] = useState(false);
+    const [likeEnabled, setLikeEnabled] = useState(false);
     const [success,setsuccess] = useState(false);
 
     const handleChange = (e)=>{
+        
         const changedField = e.target.name;
         const newVal = e.target.value;
+        
         setFormData(currData=>{
             return {...currData,
             [changedField]:newVal,
             };
         });
     }
+    const handleFeatureChange = (e)=>{
+        const temp = [volumeControlEnabled, productDrawerEnabled, reportEnabled, likeEnabled];
+        
+        if(e.target.id === 'volumeControlEnabled') temp[0] = !temp[0]
+        else if(e.target.id === 'productDrawerEnabled') temp[1] = !temp[1];
+        else if(e.target.id ==='reportEnabled') temp[2] = !temp[2]
+        else temp[3] = !temp[3]
+        
+
+        setVolumeControlEnabled(temp[0]); setProductDrawerEnabled(temp[1]); setReportEnabled(temp[2]); setLikeEnabled(temp[3]);
+        
+        
+    }
+
 
     const handleClick =  async (e)=>{
         e.preventDefault();
-        console.log(formData);
-
-        const res = await fetch('https://649f0fa3245f077f3e9d4cf3.mockapi.io/Tenants/', {
+        
+        const newdata = {
+            ...formData,features:{volumeControlEnabled:volumeControlEnabled, productDrawerEnabled:productDrawerEnabled, reportEnabled:reportEnabled, likeEnabled:likeEnabled}
+        }
+        console.log(JSON.stringify(newdata))
+        const res = await fetch('https://649ebb2f245f077f3e9cd0c1.mockapi.io/Tenants/', {
             method: 'POST',
             headers: { 'content-type': 'application/json' },
-            body: JSON.stringify(formData)
+            body: JSON.stringify(newdata)
         })
         const response = await res.json()
         console.log(response)
         setsuccess(true)
-        setFormData({
-            name:"",
-            status:true,
-            domain:""
-        })
-        navigate('/dashboard/tenants');
+        
         
 
     }
@@ -51,7 +70,7 @@ const CreateTenant = ()=>{
                         {/* <img alt=""src='/images/download.png' width={'45'} height={'25'}></img> */}
                     </button>
                     <div className='modal fade' id="modal123">
-                        <div className='modal-dialog modal-dialog-centered modal-lg'>
+                        <div className='modal-dialog modal-dialog-centered'>
                             <div className='modal-content text-center'>
                                 <div className='modal-header'>
                                     <h3>Add Tenant</h3>
@@ -76,7 +95,7 @@ const CreateTenant = ()=>{
                                             
                                             />
                                         </div>
-                                        <div className="form-group mt-3">
+                                        <div className="form-group">
                                             <label>Domain : </label>
                                             <input
                                                 type="text"
@@ -87,6 +106,25 @@ const CreateTenant = ()=>{
                                             
                                             />
                                             
+                                        </div>
+
+                                        <h3>Features:</h3>
+
+                                        <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" name="" role="switch" id="volumeControlEnabled" checked = {volumeControlEnabled} onChange={handleFeatureChange}/>
+                                        <h7>Volume Control</h7>
+                                        </div>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="productDrawerEnabled" checked = {productDrawerEnabled} onChange={handleFeatureChange}/>
+                                            <h7>Product Drawer</h7>
+                                        </div>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="reportEnabled" checked = {reportEnabled} onChange={handleFeatureChange}/>
+                                            <h7>Report Enabled</h7>
+                                        </div>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="likeEnabled" checked = {likeEnabled} onChange={handleFeatureChange}/>
+                                            <h7>Like Enabled</h7>
                                         </div>
                                         {success && <p style={{color:'green'}}>Created</p>}
                                         
